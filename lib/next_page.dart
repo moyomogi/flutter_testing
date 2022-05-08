7ef8aaf70db0e9817eae99104b458e6242351a72
Nisk36 <81093941+Nisk36@users.noreply.github.com>
2022-05-08 15:04:09 +0900

    [8]までの進捗

---
+++ lib/next_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//新しいページは新しいdartファイル作成により実現
+class NextPage extends StatefulWidget {
+  @override
+  _NextPageState createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
